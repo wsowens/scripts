6a9594aac4e143c1d544463e27a1d1f72393153c
wsowens <wowens@ufl.edu>
2020-01-29 10:05:22 -0500

    updated prefix and unprefix

--- bash/prefix.sh
+++ bash/prefix.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+if [[ ${#@} -lt 2 ]]
+then
+    >&2 echo -e "Usage: prefix [prefix] [file1] ... [fileN]"
+    >&2 echo -e "Applies a prefix to each file."
+    exit
+fi
+
 prefix=$1
+for file in ${@:2}
+do
+    test=${prefix}${file}
+    if [[ -e "${test}" ]]
+    then
+        >&2 echo -e "Error: a file named ${test} already exists. Aborting."
+        exit
+    fi
+done
+
 for file in ${@:2}
 do
     mv $file "${prefix}${file}"
--- bash/unprefix.sh
+++ bash/unprefix.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+if [[ ${#@} -lt 2 ]]
+then
+    >&2 echo -e "Usage: unprefix [prefix] [file1] ... [fileN]"
+    >&2 echo -e "Removes a prefix from each file."
+    exit
+fi
+
 prefix=$1
+for file in ${@:2}
+do
+    test=${file#${prefix}}
+    if [[ -e "${test}" && "${test}" != "${file}" ]]
+    then
+        >&2 echo -e "Error: a file named ${test} already exists. Aborting."
+        exit
+    fi
+done
+
 for file in ${@:2}
 do
     mv $file "${file#${prefix}}"
