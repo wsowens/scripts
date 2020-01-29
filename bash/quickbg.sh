0b8aa08aab5207706685bb12c535e72b521d0d2e
wsowens <wowens@ufl.edu>
2020-01-29 10:05:56 -0500

    added error-checking to quickfilter/quickbg

--- bash/quickbg.sh
+++ bash/quickbg.sh
@@ -1,4 +1,4 @@
-# /usr/bin/bash 
+#!/usr/bin/bash 
 
 # Copyright 2019 William Owens
 # 
@@ -19,18 +19,17 @@ then
     echo "Usage: quickbg [chrom.sizes] [bedGraph1] ... [bedGraphN] "
     exit -1
 fi
-if [[ $HPC_DIBIGTOOLS_DIR == "" ]]
+
+bedGraphToBigWig=$(which bedGraphToBigWig 2> /dev/null)
+
+if [[ $? != 0 ]]
 then
-   echo "Error. Must load dibig_tools module." 1>&2
+   echo "Error. Cannot find bedGraphToBigWig. Please load ucsc module." 1>&2
    exit -1
-else
-    # storing the path of bedGraphToBigWig
-    # if you have a different copy of bedGraphToBigWig, change the path
-    # and delete the check above
-    bedGraphToBigWig="${HPC_DIBIGTOOLS_DIR}/lib/ucsc/bedGraphToBigWig"
 fi
+
 chroms=$1
-for file in ${@:2}
+for file in "${@:2}"
 do
     srun --ntasks=1 --mem=20gb -t 10:00:00 $bedGraphToBigWig $file $chroms ${file%.bedGraph}.bw  &
 done
--- bash/quickfilter.sh
+++ bash/quickfilter.sh
@@ -24,11 +24,18 @@ then
     exit -1
 fi
 
-# storing the path of filtercov, the Rust program from wowens
-# if you have a different copy of filtercov, just edit as appropriate
-filtercov_bin="/ufrc/riva/wowens/apps/rust/filtercov/target/release/filtercov"
-coverage=$1
+which filtercov &> /dev/null
+
+if  [ "$?" -gt 0 ]
+then
+    >&2 echo "'filtercov' not found in path. This script requires filtercov."
+    >&2 echo "You can find filtercov here: https://github.com/xeranimus/filtercov/"
+fi
+
+
+coverage="$1"
 for filename in ${@:2}
 do
-	srun --ntasks=1 --mem=5gb -t 10:00:00 $filtercov_bin "${coverage}" $filename &
+	echo "srun --ntasks=1 --mem=5gb -t 10:00:00 filtercov \"${coverage}\" $filename &"
+	srun --ntasks=1 --mem=5gb -t 10:00:00 filtercov "${coverage}" $filename &
 done
