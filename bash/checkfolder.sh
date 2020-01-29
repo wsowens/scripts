8d717d36c1cc4f6d191e529b1e22d7341e6c067e
wsowens <wowens@ufl.edu>
2020-01-29 10:05:28 -0500

    added license message

--- bash/checkfolder.sh
+++ bash/checkfolder.sh
@@ -2,6 +2,25 @@
 
 #check that two folders are equivalent
 
+# Copyright 2019 William Owens
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# removing the unaligned reads at the bottom
+# then cutting out the third column (mapped reads)
+# then summing with awk
+
+
 if [[ "${#@}" -lt 2 ]]
 then
     >&2 echo -e "Usage: checkfolder [folder1] [folder2]"
